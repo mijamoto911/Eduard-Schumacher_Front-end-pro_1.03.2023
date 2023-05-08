52210ead11b3c4a0631b4ddb9ab59789b9e95901
Shumakher Eduard <wendalin@protonmail.ch>
2023-05-08 21:39:20 +0200

    lesson-16

---
+++ lesson_16/script.js
@@ -0,0 +1,9 @@
+function showDiv() {
+    const div = document.getElementById("myDiv");
+    div.style.display = "block";
+}
+
+function hideDiv() {
+    const div = document.getElementById("myDiv");
+    div.style.display = "none";
+}
